e8bb997da941cd9dcba73dfcce538ffe4702a555
ClutchoQ <1057944298@qq.com>
2025-03-03 11:30:18 +0800

    第一版在线生成sitemap

---
+++ netlify/functions/generate-sitemap.js
@@ -0,0 +1,36 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+exports.handler = async (event) => {
+    try {
+        const { url } = JSON.parse(event.body);
+        const response = await axios.get(url);
+        const $ = cheerio.load(response.data);
+
+        // 简单示例：提取所有链接
+        const links = [];
+        $('a').each((i, elem) => {
+            const href = $(elem).attr('href');
+            if (href && href.startsWith('/')) links.push(new URL(href, url).href);
+        });
+
+        // 生成XML
+        const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    ${links.map(link => `
+    <url>
+        <loc>${link}</loc>
+        <lastmod>${new Date().toISOString()}</lastmod>
+        <changefreq>daily</changefreq>
+        <priority>0.8</priority>
+    </url>`).join('')}
+</urlset>`;
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ xmlContent: xml })
+        };
+    } catch (error) {
+        return { statusCode: 500, body: JSON.stringify({ error: error.message }) };
+    }
+};
